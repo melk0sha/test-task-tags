afccf4f67c62eba71b073fe2e2b3e1b4a0b705da
Dziyana Ivanova <Dziyana_Ivanova@epam.com>
2020-07-20 02:03:08 +0300

    feat: add DND component

--- src/components/DragAndDrop/index.js
+++ src/components/DragAndDrop/index.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { FileDrop } from "react-file-drop";
+import "./index.scss";
 
-function DragAndDrop() {
-  return <div></div>;
-}
+const DragAndDrop = ({ handleSrc }) => {
+  const [dropText, setDropText] = useState("Drop file here!");
+
+  const handleDrop = (files) => {
+    const [image] = files;
+    setDropText("Drop file here!");
+
+    if (image) {
+      const reader = new FileReader();
+
+      reader.addEventListener("load", () => {
+        handleSrc(reader.result);
+      });
+
+      reader.readAsDataURL(image);
+    }
+  };
+
+  const handleDragOver = () => {
+    setDropText("Dropping...");
+  };
+
+  return (
+    <div className="dnd">
+      <FileDrop
+        className="dnd-input"
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        {dropText}
+      </FileDrop>
+    </div>
+  );
+};
 
 export default DragAndDrop;
